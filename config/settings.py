6afa929bf30650e0e57cb9fdc6d19c8cdb1903fe
Zhorik Zeniuk <ZhorikZeniuk@gmail.ru>
2023-07-25 11:42:46 +0200

    created models

--- config/settings.py
+++ config/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import json
 import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+with open('prvt.json') as file:
+    db_pass = json.load(file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -77,11 +80,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mailsender',
+        'USER': 'ZhorikZeniuk',
+        'PASSWORD': db_pass['pass'],
+        'HOST': '',
+        'PORT': 5433,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- mailsender/models.py
+++ mailsender/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Customer(models.Model):
+    """
+    Клиент сервиса:
+        контактный email,
+        ФИО,
+        комментарий.
+    """
+    email = models.EmailField(max_length=254, unique=True, verbose_name='Почта')
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    middle_name = models.CharField(max_length=50, **NULLABLE, verbose_name='Отчество')
+
+
+class Mailing(models.Model):
+    """
+    Рассылка (настройки):
+        время рассылки;
+        периодичность: раз в день, раз в неделю, раз в месяц;
+        статус рассылки: завершена, создана, запущена.
+    """
+    mailing_time = models.TimeField(auto_now_add=True, verbose_name='Время рассылки')
+    once = models.BooleanField(default=True, verbose_name='Единоразовая рассылка')
+    every_week = models.BooleanField(default=False, verbose_name='Рассылка раз в неделю')
+    every_month = models.BooleanField(default=False, verbose_name='Рассылка раз в месяц')
+    status = models.CharField(max_length=50, verbose_name='Статус рассылки')
+
+
+class MailText(models.Model):
+    """
+    Сообщение для рассылки:
+        тема письма,
+        тело письма.
+    """
+    topic = models.CharField(max_length=100, verbose_name='Тема рассылки')
+    message = models.TextField(verbose_name='Текст сообщения')
+
+
+class Logs(models.Model):
+    """
+    Логи рассылки:
+        дата и время последней попытки;
+        статус попытки;
+        ответ почтового сервера, если он был.
+    """
+    datetime_of_last_mailing = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время последней рассылки')
+    status = models.BooleanField(default=False, verbose_name='Статус рассылки')
